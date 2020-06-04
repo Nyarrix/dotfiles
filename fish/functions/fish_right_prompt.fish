701de98bc6b05a47c9cc82efd3eedaa73e502aa3
Nyarrix <takap.walkingliberty@gmail.com>
2020-06-04 21:48:14 +0900

    changing fish prompt

--- fish/functions/fish_right_prompt.fish
+++
@@ -1,7 +0,0 @@
-function fish_right_prompt
-	set -l is_git_dir (git rev-parse --git-dir 2> /dev/null)
-	if test -n "$is_git_dir"
-		set -l branch (git branch | grep \* | cut -d ' ' -f2)
-		echo (set_color green)\[$branch\](set_color normal)
-	end
-end
